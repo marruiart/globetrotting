2a6a9a6a18996602a1f839178524b2f8cf7a21a0
marruiart <marruiart@gmail.com>
2023-11-15 12:31:13 +0100

    add authStrapiService
    	new file:   src/app/core/models/user-credentials.interface.ts
    	new file:   src/app/core/models/user-register.interface copy.ts
    	new file:   src/app/core/services/auth/auth-strapi.service.ts

---
+++ src/app/core/services/auth/auth-strapi.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { ApiService } from '../api.service';
+import { JwtService } from './jwt.service';
+import { AuthProvider } from './auth.provider';
+import { lastValueFrom } from 'rxjs';
+import { UserCredentials } from '../../models/user-credentials.interface';
+import { Auth } from '../../models/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthStrapiService extends AuthProvider {
+
+  constructor(
+    private apiSvc: ApiService,
+    private jwtSvc: JwtService,
+  ) {
+    super();
+    this.init();
+  }
+
+  private init() {
+    this.jwtSvc.loadToken().subscribe(_ => {
+      this._isLogged.next(true);
+    });
+  }
+
+  public login(credentials: UserCredentials): Observable<void> {
+    const body: any = {
+      "identifier": credentials.username,
+      "password": credentials.password
+    }
+    return new Observable<void>(observer => {
+      this.apiSvc.post<Auth>("/api/auth/local", body)
+        .subscribe({
+          next: async auth => {
+            await lastValueFrom(this.jwtSvc.saveToken(auth.jwt))
+              .catch(err => {
+                observer.error(err);
+              });
+            this._isLogged.next(true);
+            observer.next();
+            observer.complete();
+          },
+          error: err => {
+            observer.error(err);
+          }
+        });
+    });
+  }
+
+  public register(info: Object): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  public logout(): void {
+    lastValueFrom(this.jwtSvc.destroyToken())
+      .then(_ => {
+        this._isLogged.next(false);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  public me<T>(): Observable<T> {
+    throw new Error('Method not implemented.');
+  }
+
+}

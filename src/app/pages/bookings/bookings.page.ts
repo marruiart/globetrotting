7e277ab631cc63b0070838eecd5fb58dedb60201
marruiart <marruiart@gmail.com>
2023-12-02 14:23:49 +0100

    refactor booking page

--- src/app/pages/bookings/bookings.page.ts
+++ src/app/pages/bookings/bookings.page.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable, catchError, concatMap, lastValueFrom, of, zip } from 'rxjs';
 import { UserFacade } from 'src/app/core/libs/load-user/load-user.facade';
 import { TravelAgent } from 'src/app/core/models/globetrotting/agent.interface';
+import { Booking } from 'src/app/core/models/globetrotting/booking.interface';
 import { Client } from 'src/app/core/models/globetrotting/client.interface';
+import { Destination } from 'src/app/core/models/globetrotting/destination.interface';
+import { User } from 'src/app/core/models/globetrotting/user.interface';
 import { AgentService } from 'src/app/core/services/api/agent.service';
 import { BookingsService } from 'src/app/core/services/api/bookings.service';
 import { ClientService } from 'src/app/core/services/api/client.service';
@@ -10,22 +13,20 @@ import { DestinationsService } from 'src/app/core/services/api/destinations.serv
 import { UsersService } from 'src/app/core/services/api/users.service';
 import { SubscriptionsService } from 'src/app/core/services/subscriptions.service';
 
-interface ClientTableRow {
+interface TableRow {
   destination: string,
   start: string,
   end: string,
-  travelers: number,
-  isConfirmed: boolean,
-  agentName: string | null
+  travelers: number
 }
 
-interface AgentTableRow {
-  clientName: string,
-  destination: string,
-  start: string,
-  end: string,
-  travelers: number,
+interface ClientTableRow extends TableRow {
+  agentName: string | null,
   isConfirmed: boolean
+}
+
+interface AgentTableRow extends TableRow {
+  clientName: string
 }
 
 @Component({
@@ -34,9 +35,10 @@ interface AgentTableRow {
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
-  private role: Client | TravelAgent | null = null;
-  private _bookingTable: BehaviorSubject<ClientTableRow[] | AgentTableRow[]> = new BehaviorSubject<ClientTableRow[] | AgentTableRow[]>([]);
-  public bookingTable$: Observable<ClientTableRow[] | AgentTableRow[]> = this._bookingTable.asObservable();
+  private currentUser: Client | TravelAgent | null = null;
+  private mappedBookings: TableRow[] = [];
+  private _bookingTable: BehaviorSubject<TableRow[]> = new BehaviorSubject<TableRow[]>([]);
+  public bookingTable$: Observable<TableRow[]> = this._bookingTable.asObservable();
   public data: ClientTableRow[] = [];
   public cols: any[] = [];
 
@@ -51,116 +53,134 @@ export class BookingsPage implements OnInit {
   ) {
     this.subsSvc.addSubscription({
       component: 'BookingsPage',
-      sub: this.userFacade.currentSpecificUser$.subscribe(role => {
-        this.role = role;
+      sub: this.userFacade.currentSpecificUser$.subscribe(currentUser => {
+        this.currentUser = currentUser;
       })
     })
   }
 
 
   async ngOnInit() {
-    if (this.role?.type == 'AUTHENTICATED') {
-      this.displayClientBookings();
-    } else if (this.role?.type == 'AGENT') {
-      this.displayAgentBookings();
-    }
-  }
-
-  private async displayClientBookings() {
-    let mappedBookings: ClientTableRow[] = [];
     let bookings = await lastValueFrom(this.bookingsSvc.getAllUserBookings());
     bookings.forEach(booking => {
-      const agent$ = booking.agent_id ? this.agentSvc.getAgent(booking.agent_id) : of(null);
-      const destination$ = this.destinationSvc.getDestination(booking.destination_id);
-
-      this.subsSvc.addSubscription({
-        component: 'BookingsPage',
-        sub: zip(agent$, destination$).pipe(
-          concatMap(([agent, destination]): Observable<ClientTableRow | null> => {
-            const userAgent$ = agent ? this.usersSvc.getAgentUser(agent.user_id) : of(null);
-
-            return userAgent$.pipe(
-              concatMap(userAgent => {
-                mappedBookings.push({
-                  destination: destination ? destination.name : 'Desconocido',
-                  start: booking.start,
-                  end: booking.end,
-                  travelers: booking.travelers,
-                  agentName: `${userAgent?.name ?? "-"} ${userAgent?.surname ?? ""}`,
-                  isConfirmed: booking.isConfirmed ?? false
-                })
-                return mappedBookings;
-              }), catchError(err => {
-                console.error(err)
-                return of(null);
-              }))
-
-          }), catchError(err => {
-            console.error(err)
-            return of(null);
-          })
-        ).subscribe({
-          next: _ => this._bookingTable.next(mappedBookings),
-          error: err => console.error(err)
-        })
-      })
+      if (this.currentUser?.type == 'AUTHENTICATED') {
+        this.displayClientBookings(booking);
+      } else if (this.currentUser?.type == 'AGENT') {
+        this.displayAgentBookings(booking);
+      }
     })
-
-    this.cols = [
-      { field: 'destination', header: 'Destino' },
-      { field: 'start', header: 'Fecha de inicio' },
-      { field: 'end', header: 'Fecha de fin' },
-      { field: 'travelers', header: 'Número de viajeros' },
-      { field: 'isConfirmed', header: 'Estado' },
-      { field: 'agentName', header: 'Agente asignado' }
-    ];
+    this.cols = this.getCols();
   }
 
-  private async displayAgentBookings() {
-    let mappedBookings: AgentTableRow[] = [];
-    let bookings = await lastValueFrom(this.bookingsSvc.getAllUserBookings());
-    bookings.forEach(booking => {
-      const client$ = booking.client_id ? this.clientSvc.getClient(booking.client_id) : of(null);
-      const destination$ = this.destinationSvc.getDestination(booking.destination_id);
+  private getCols() {
+    if (this.currentUser?.type == 'AUTHENTICATED') {
+      return [
+        { field: 'destination', header: 'Destino' },
+        { field: 'start', header: 'Fecha de inicio' },
+        { field: 'end', header: 'Fecha de fin' },
+        { field: 'travelers', header: 'Número de viajeros' },
+        { field: 'isConfirmed', header: 'Estado' },
+        { field: 'agentName', header: 'Agente asignado' }
+      ]
+    } else if (this.currentUser?.type == 'AGENT') {
+      return [
+        { field: 'clientName', header: 'Cliente' },
+        { field: 'destination', header: 'Destino' },
+        { field: 'start', header: 'Fecha de inicio' },
+        { field: 'end', header: 'Fecha de fin' },
+        { field: 'travelers', header: 'Número de viajeros' },
+      ]
+    }
+    return [];
+  }
 
-      zip(client$, destination$).pipe(
-        concatMap(([client, destination]): Observable<AgentTableRow | null> => {
-          const userClient$ = client ? this.usersSvc.getClientUser(client.user_id) : of(null);
+  private mapTableRow(user: User | null, booking: Booking, destination: Destination): TableRow | null {
+    if (this.currentUser?.type == 'AUTHENTICATED') {
+      const clientTableRow: ClientTableRow = {
+        destination: destination ? destination.name : 'Desconocido',
+        start: booking.start,
+        end: booking.end,
+        travelers: booking.travelers,
+        agentName: `${user?.name ?? "-"} ${user?.surname ?? ""}`,
+        isConfirmed: booking.isConfirmed ?? false
+      }
+      return clientTableRow;
+    } else if (this.currentUser?.type == 'AGENT') {
+      const agentTableRow: AgentTableRow = {
+        destination: destination ? destination.name : 'Desconocido',
+        start: booking.start,
+        end: booking.end,
+        travelers: booking.travelers,
+        clientName: `${user?.name ?? "-"} ${user?.surname ?? ""}`
+      }
+      return agentTableRow;
+    }
+    return null;
+  }
 
-          return userClient$.pipe(
-            concatMap(userClient => {
-              mappedBookings.push({
-                destination: destination ? destination.name : 'Desconocido',
-                start: booking.start,
-                end: booking.end,
-                travelers: booking.travelers,
-                clientName: `${userClient?.name ?? "-"} ${userClient?.surname ?? ""}`,
-                isConfirmed: booking.isConfirmed ?? false
-              })
-              return mappedBookings;
+  private async displayClientBookings(booking: Booking) {
+    const agent$ = booking.agent_id ? this.agentSvc.getAgent(booking.agent_id) : of(null);
+    const destination$ = this.destinationSvc.getDestination(booking.destination_id);
+
+    this.subsSvc.addSubscription({
+      component: 'BookingsPage',
+      sub: zip(agent$, destination$).pipe(
+        concatMap(([agent, destination]): Observable<TableRow | null> => {
+          const userAgent$ = agent ? this.usersSvc.getAgentUser(agent.user_id) : of(null);
+
+          return userAgent$.pipe(
+            concatMap(userAgent => {
+              let clientTableRow = this.mapTableRow(userAgent, booking, destination);
+              if (clientTableRow) {
+                this.mappedBookings.push(clientTableRow)
+              }
+              return this.mappedBookings;
             }), catchError(err => {
-              console.error(err)
+              console.error(err);
               return of(null);
             }))
 
         }), catchError(err => {
-          console.error(err)
+          console.error(err);
           return of(null);
         })
       ).subscribe({
-        next: _ => this._bookingTable.next(mappedBookings),
+        next: _ => this._bookingTable.next(this.mappedBookings),
         error: err => console.error(err)
-      });
-    })
+      })
+    });
+  }
 
-    this.cols = [
-      { field: 'clientName', header: 'Cliente' },
-      { field: 'destination', header: 'Destino' },
-      { field: 'start', header: 'Fecha de inicio' },
-      { field: 'end', header: 'Fecha de fin' },
-      { field: 'travelers', header: 'Número de viajeros' },
-      { field: 'isConfirmed', header: 'Estado' },
-    ];
+  private async displayAgentBookings(booking: Booking) {
+    const client$ = booking.client_id ? this.clientSvc.getClient(booking.client_id) : of(null);
+    const destination$ = this.destinationSvc.getDestination(booking.destination_id);
+    this.subsSvc.addSubscription({
+      component: 'BookingsPage',
+      sub: zip(client$, destination$).pipe(
+        concatMap(([client, destination]): Observable<TableRow | null> => {
+          const userClient$ = client ? this.usersSvc.getClientUser(client.user_id) : of(null);
+
+          return userClient$.pipe(
+            concatMap(userClient => {
+              let agentTableRow = this.mapTableRow(userClient, booking, destination);
+              if (agentTableRow) {
+                this.mappedBookings.push(agentTableRow)
+              }
+              return this.mappedBookings;
+            }), catchError(err => {
+              console.error(err);
+              return of(null);
+            }))
+
+        }), catchError(err => {
+          console.error(err);
+          return of(null);
+        })
+      ).subscribe({
+        next: _ => this._bookingTable.next(this.mappedBookings),
+        error: err => console.error(err)
+      })
+    });
   }
 
   ngOnDestroy() {

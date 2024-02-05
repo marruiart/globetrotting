971ef6dd9ecba0ce0c1a12ffce25d912cb6f27c2
marruiart <marruiart@gmail.com>
2024-02-05 20:54:56 +0100

    Fix: update reducer to have ext_id and specifid_id

--- src/app/core/+state/auth/auth.reducer.ts
+++ src/app/core/+state/auth/auth.reducer.ts
@@ -1,17 +1,18 @@
 import { createReducer, on } from '@ngrx/store';
 import * as AuthAction from './auth.actions';
 import { Backend, Firebase } from 'src/environments/environment';
-import { Role } from '../../models/globetrotting/user.interface';
+import { ClientUser, Role, User } from '../../models/globetrotting/user.interface';
 import { ClientBooking, ClientFavDestination } from '../../models/globetrotting/client.interface';
 import { Booking } from '../../models/globetrotting/booking.interface';
+import { isType } from '../../utilities/utilities';
 
 export const AUTH_FEATURE_KEY = 'auth'
 
 export type AuthState = {
     isLogged: boolean,
     user_id: (string | number) | null,
-    ext_id?: number,
-    specific_id?: number,
+    ext_id: (string | number) | null,
+    specific_id: (string | number) | null,
     role: Role | null,
     nickname: string,
     email: string,
@@ -27,6 +28,8 @@ export type AuthState = {
 export const initialState: AuthState = {
     isLogged: false,
     user_id: null,
+    ext_id: null,
+    specific_id: null,
     nickname: '',
     email: '',
     role: null,
@@ -38,7 +41,7 @@ export const authReducer = createReducer(
     on(AuthAction.loginSuccess, (state) => ({ ...state, isLogged: true, error: null })),
     on(AuthAction.loginFailure, (state, { error }) => ({ ...state, isLogged: false, role: null, user_id: null, error: error })),
     on(AuthAction.assignUid, (state, { user_id }) => ({ ...state, isLogged: true, user_id: user_id, error: null })),
-    on(AuthAction.assignUserSuccess, (state, { user }) => ({ ...state, isLogged: true, nickname: user.nickname, role: user.role, user_id: (user.user_id as (Backend extends Firebase ? string : number)), error: null })), // TODO map all the user input
+    on(AuthAction.assignUserSuccess, (state, { user }) => assignUserMapping(state, user)), // TODO map all the user input
     on(AuthAction.assignUserFailure, (state, { error }) => ({ ...state, error: error })),
     on(AuthAction.logout, (state) => ({ ...state })),
     on(AuthAction.logoutSuccess, (state) => ({ ...state, isLogged: false, role: null, user_id: null, error: null })),
@@ -47,3 +50,23 @@ export const authReducer = createReducer(
     on(AuthAction.registerSuccess, (state) => ({ ...state, isLogged: true, error: null })),
     on(AuthAction.registerFailure, (state, { error }) => ({ ...state, isLogged: false, role: null, user_id: null, error: error }))
 );
+
+function assignUserMapping(state: AuthState, user: User): AuthState {
+    return {
+        ...state,
+        isLogged: true,
+        user_id: (user.user_id as (Backend extends Firebase ? string : number)),
+        ext_id: user.ext_id ?? user.user_id,
+        specific_id: user.specific_id ?? user.user_id,
+        nickname: user.nickname,
+        email: user.email,
+        role: user.role,
+        avatar: user.avatar,
+        name: user.name,
+        surname: user.surname,
+        age: user.age,
+        bookings: user.bookings,
+        favorites: isType<ClientUser>(user) ? user.favorites : undefined,
+        error: null
+    }
+}

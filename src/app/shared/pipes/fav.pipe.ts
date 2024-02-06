dce188eba06c349b38f53f1aedcfc0846d493851
marruiart <marruiart@gmail.com>
2024-02-06 07:18:57 +0100

    Feature: showing user favorites (not finished)

--- src/app/shared/pipes/fav.pipe.ts
+++ src/app/shared/pipes/fav.pipe.ts
@@ -1,17 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { TravelAgent } from 'src/app/core/models/globetrotting/agent.interface';
-import { Client } from 'src/app/core/models/globetrotting/client.interface';
+import { ClientFavDestination } from 'src/app/core/models/globetrotting/client.interface';
 import { Destination } from 'src/app/core/models/globetrotting/destination.interface';
+import { User } from 'src/app/core/models/globetrotting/user.interface';
 
 @Pipe({
   name: 'fav'
 })
 export class FavPipe implements PipeTransform {
 
-  transform(destinations: Destination[], client: any | null): Destination[] { // TODO clases de esto
-    if (client?.type == 'AUTHENTICATED') {
+  transform(destinations: Destination[], client: User | null): Destination[] {
+    if (client?.role == 'AUTHENTICATED') {
       destinations.map(destination => {
-        return destination["fav"] = client.favorites.reduce((prev: any, fav: { destination_id: number; }) => prev || fav.destination_id == destination.id, false) ?? false;
+        return destination['fav'] = client.favorites.reduce((prev: any, fav: ClientFavDestination) => prev || fav.destination_id == destination.id, false) ?? false;
       })
     }
     return destinations;
